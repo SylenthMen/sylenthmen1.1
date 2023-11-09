81d25c16571c723c06fe38a6e479d2111355e209
sylenthmen <manuelene26@gmail.com>
2023-11-09 17:35:39 -0600

    se agrega pagina de entrada al blog asi mismo se corrigen errores en colores y se modifica la fuente de todo el sitio

---
+++ blog/styleblog/entradablog.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const entriesData = [
+      { id: 1, title: "Contruccion de este sitio", content: "Contenido de la Entrada 1" },
+      { id: 2, title: "Entrada 2", content: "Contenido de la Entrada 2" },
+      { id: 3, title: "Entrada 3", content: "Contenido de la Entrada 3" },
+      { id: 4, title: "Entrada 4", content: "Contenido de la Entrada 4" },
+      { id: 5, title: "Entrada 5", content: "Contenido de la Entrada 5" },
+      // Agrega más entradas según sea necesario
+    ];
+
+    const entriesContainer = document.getElementById("entries-container");
+    const toggleModeButton = document.getElementById("toggle-mode");
+
+    function renderEntries() {
+      entriesContainer.innerHTML = "";
+
+      entriesData.forEach((entry) => {
+        const entryElement = document.createElement("div");
+        entryElement.classList.add("entry");
+
+        const titleElement = document.createElement("h2");
+        const linkElement = document.createElement("a");
+
+        linkElement.href = `entrada${entry.id}.html`;
+        linkElement.textContent = entry.title;
+        titleElement.appendChild(linkElement);
+
+        const contentElement = document.createElement("p");
+        contentElement.textContent = entry.content;
+
+        entryElement.appendChild(titleElement);
+        entryElement.appendChild(contentElement);
+
+        entriesContainer.appendChild(entryElement);
+      });
+    }
+
+    function toggleDarkMode() {
+      document.body.classList.toggle("dark-mode");
+    }
+
+    toggleModeButton.addEventListener("click", toggleDarkMode);
+
+    renderEntries();
+  });
